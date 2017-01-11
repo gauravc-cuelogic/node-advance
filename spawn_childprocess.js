526296aff746b7c23d8c00d0fd8db780cc34bb8e
Gaurav <gaurav.chauriya@cuelogic.com>
2017-01-11 22:23:58 +0530

    spwan

--- spawn_childprocess.js
+++ spawn_childprocess.js
@@ -1,1 +1,12 @@
-var spa
+var spawn = require('child_process').spawn;
+
+var sp = spawn("node", ["event_handiling"]);
+
+sp.stdout.on('data',function(data){
+	console.log(`STDOUT: ${data} `);
+});
+
+sp.on('close', function (){
+	console.log('child ended');
+	process.exit();
+});
